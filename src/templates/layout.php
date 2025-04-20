22adc2dfdf416447906d80354e6e493ac31a1560
lyucean <lyucean@gmail.com>
2025-04-20 15:29:04 +0300

    nit

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Совпадём' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css?v=<?= filemtime('css/styles.css') ?>">
+    <link rel="stylesheet" href="/css/styles.css?v=<?= filemtime('css/styles.css') ?>">
     
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
